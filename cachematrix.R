697cf6295d56d00ce24078a3d0944880ef71b750
Rae A <me>
2023-11-23 20:02:46 -0500

    Submitting Week 3 assignment
    Implements matrix cache and inverse calculation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+ ##Create special matrix object with functions that allow for caching of matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    cacheinverse <- NULL
+    getmatrix <- function() {
+        x
+    }
+    setinverse <- function(inverse) {
+        cacheinverse <<- inverse
+    }
+    getinverse <- function() {
+        cacheinverse
+    }
+    list(setinverse=setinverse, getmatrix=getmatrix, getinverse=getinverse)
 
 }
 
+ ##Returns an inverse of the matrix stored in special matrix object created in makeCacheMatrix()
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrixobject, ...) {
+    inverse <- matrixobject$getinverse()
+    if(!is.null(inverse))  {
+        return(inverse)
+    }
+    data <- matrixobject$getmatrix()
+    inverse <- solve(data, ...)
+    matrixobject$setinverse(inverse)
+    inverse
 }
